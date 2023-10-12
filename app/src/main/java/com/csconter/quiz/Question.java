76591e239e35a3bb38d9e070917caacc80bd7b7f
edobrzycka <edobrzycka2803@gmail.com>
2023-10-12 15:41:34 +0200

    Add new files

---
+++ app/src/main/java/com/csconter/quiz/Question.java
@@ -0,0 +1,19 @@
+package com.csconter.quiz;
+
+public class Question {
+    private int questionText;
+    private boolean correctAnswer;
+
+    public Question(int questionText, boolean correctAnswer) {
+        this.questionText = questionText;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public int getQuestionText() {
+        return questionText;
+    }
+
+    public boolean isCorrect() {
+        return correctAnswer;
+    }
+}
